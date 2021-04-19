93dede4bee359f6921d88f44891e6a988ad672f5
Nimoscmp <nimos.cmp@gmail.com>
2021-04-19 16:29:59 -0500

    Modify app to include context and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Form from "./components/Form";
+import Header from "./components/Header";
+import CategoriesProvider from "./context/CategoriesContext";
+import RecipesProvider from "./context/RecipesContext";
 
 
 function App() {
   return (
-    <>
-      
-    </>
+    <RecipesProvider>
+        <CategoriesProvider>
+            <Header />
+            <Form />
+        </CategoriesProvider>
+    </RecipesProvider>      
   );
 }
 
